800fa5e7b379e3a2013c0926e725ecd204d37198
Neef Rehman <neef.rehman@gmail.com>
2020-05-10 18:31:25 +0100

    fix: better type-safety and overload management

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,15 @@
 /**
- * A multidimensional array returned by `makeMatrix`, typed recusively to ensure each
- * point in the matrix has the same type as the function's `initialValues` parameter.
+ * A multidimensional array returned by `makeMatrix` when over 4 dimensions are required.
  */
-export type Matrix<T> = T[] | Matrix<T>[];
+export type Matrix<T> = T[][][][][] | Matrix<T>[];
 
 /**
  * Returns a matrix (multi-dimensional array) with your desired dimensions and values.
  *
- * Both numbers and number arrays are supported to create the matrix. If a number is
- * passed as the `dimensions` parameter, the smallest array with that number of
- * dimensions is returned. If an array is passed, the returned matrix will be of
- * those exact dimensions. For example, passing in `[3, 2, 5]` will result in a
- * three-dimensional array that is 3x2x5 in size.
+ * If a number is passed to the `dimensions` parameter, the smallest array with
+ * that number of dimensions is returned. If an array is passed, the returned
+ * matrix will be of those exact dimensions. For example, passing in `[3, 2, 5]`
+ * will result in a three-dimensional array that is 3x2x5 in size.
  *
  * All points within the matrix can be initialised to a specific value with the
  * optional `initialValues` parameter.
@@ -20,30 +18,31 @@ export type Matrix<T> = T[] | Matrix<T>[];
  * @param initialValues - The value that each point in the matrix will initialise to. Can be anything. Defaults to `null`.
  * @returns A matrix of `N` dimensions, with each point initialised to the `initialValues` parameter.
  */
+function makeMatrix<T>(dimensions: 1 | [number], initialValues?: T): T[];
+
+function makeMatrix<T>(
+    dimensions: 2 | [number, number],
+    initialValues?: T
+): T[][];
+
+function makeMatrix<T>(
+    dimensions: 3 | [number, number, number],
+    initialValues?: T
+): T[][][];
+
+function makeMatrix<T>(
+    dimensions: 4 | [number, number, number, number],
+    initialValues?: T
+): T[][][][];
+
 function makeMatrix<T>(
     dimensions: number | number[],
     initialValues?: T
 ): Matrix<T>;
 
-// TODO: Find way to get more specific return types with below overloads:
-// https://github.com/microsoft/TypeScript/issues/38357
-// const mat = makeMatrix(3); // <- type should be number[][][], not Matrix
-
-// function makeMatrix<T>(dimensions: 1 | [number], initialValues?: T): T[];
-
-// function makeMatrix<T>(
-//     dimensions: 2 | [number, number],
-//     initialValues?: T
-// ): T[][];
-
-// function makeMatrix<T>(
-//     dimensions: 3 | [number, number, number],
-//     initialValues?: T
-// ): T[][][];
-
 function makeMatrix<T>(
     dimensions: number | number[],
-    initialValues: T = null
+    initialValues: T | null = null
 ): Matrix<T> {
     let currentDimensionWidth: number;
     let remainingDimensions: number | number[];
@@ -71,3 +70,11 @@ function makeMatrix<T>(
 }
 
 export default makeMatrix;
+
+// TODO: Find a way to get generic overload suggested first?
+// AND have overload become more specfic as matching parameters are entered
+// (don't think it's possible 😢)
+// https://github.com/microsoft/TypeScript/issues/38357
+
+// TODO: Need a better generic type? This one doesn't work
+// Matrix isn't very safe
