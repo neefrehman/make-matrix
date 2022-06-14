6e0e95da6c1b49b24d3cc7d9234f4443e5084aa9
Neef Rehman <neef.rehman@gmail.com>
2022-06-14 15:37:28 +0100

    chore: refactor tests to use each blocks

---
+++ src/__tests__/index.test.ts
@@ -0,0 +1,69 @@
+import makeMatrix from "../index";
+
+describe("validation", () => {
+    test("should error when a float is used as a dimension", () => {
+        expect(() => makeMatrix([1, 0.2, 3])).toThrow(TypeError);
+    });
+});
+
+const nDimensions = [1, 2, 3, 4, 5];
+const specificDimensions = [[6], [1, 1], [3, 2, 3], [1, 4, 5, 2], [2, 4, 2, 4, 2]];
+
+const describeCases = [
+    { argType: "N", testCases: nDimensions.map(n => [n, n]) },
+    { argType: "specific", testCases: specificDimensions.map(n => [n, n]) }
+];
+
+describe.each(describeCases)("arrays of $argType dimensions —", ({ testCases }) => {
+    describe("No initial values:", () => {
+        test.each(testCases)("%p", dimensions => {
+            // @ts-ignore — `Type instantiation is excessively deep...`
+            expect(makeMatrix(dimensions)).toMatchSnapshot();
+        });
+    });
+
+    describe("string initial values:", () => {
+        test.each(testCases)("%p", dimensions => {
+            // @ts-ignore — `Type instantiation is excessively deep...`
+            expect(makeMatrix(dimensions, "string")).toMatchSnapshot();
+        });
+    });
+
+    describe("number initial values:", () => {
+        test.each(testCases)("%p", dimensions => {
+            // @ts-ignore — `Type instantiation is excessively deep...`
+            expect(makeMatrix(dimensions, 0)).toMatchSnapshot();
+        });
+    });
+
+    describe("callback initial values — math:", () => {
+        test.each(testCases)("%p", dimensions => {
+            // @ts-ignore — `Type instantiation is excessively deep...`
+            expect(makeMatrix(dimensions, () => 10 - 2)).toMatchSnapshot();
+        });
+    });
+
+    describe("self aware callback initial values — vector:", () => {
+        test.each(testCases)("%p", dimensions => {
+            // @ts-ignore — `Type instantiation is excessively deep...`
+            expect(makeMatrix(dimensions, vector => vector)).toMatchSnapshot();
+        });
+    });
+
+    describe("self aware callback initial values — joined:", () => {
+        test.each(testCases)("%p", dimensions => {
+            expect(
+                makeMatrix(dimensions, vector => `my position is ${vector.join()}`)
+            ).toMatchSnapshot();
+        });
+    });
+
+    describe("self aware callback initial values — object conversion:", () => {
+        test.each(testCases)("%p", dimensions => {
+            expect(
+                // @ts-ignore — `Type instantiation is excessively deep...`
+                makeMatrix(dimensions, ([x, y, z, a, b, c]) => ({ x, y, z, a, b, c }))
+            ).toMatchSnapshot();
+        });
+    });
+});
--- src/__tests__/non-integer-error.test.ts
+++
@@ -1,6 +0,0 @@
-import makeMatrix from "../index";
-
-// Test the throwing of a TypeError if a non-integer is passed in `dimensions`
-test("Array error test - When non-integer is used", () => {
-    expect(() => makeMatrix([1, 0.2, 3])).toThrow(TypeError);
-});
