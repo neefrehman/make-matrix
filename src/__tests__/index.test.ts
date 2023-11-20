f6abb4a18361149200d42734a0f6fb110f883d70
Neef Rehman <neef.rehman@gmail.com>
2023-11-20 16:11:24 -0500

    ci(test): replace jest with vitest (#23)
    vitest is faster, requires less configuration, and has a smaller dependency footprint

--- src/__tests__/index.test.ts
+++ src/__tests__/index.test.ts
@@ -1,3 +1,4 @@
+import { expect, test, describe } from "vitest";
 import { makeMatrix } from "../index";
 
 const testCases = [[6], [1, 1], [3, 2, 3], [1, 4, 5, 2], [2, 4, 2, 4, 2]];
--- src/__tests__/types.test.ts
+++ src/__tests__/types.test.ts
@@ -1,19 +1,18 @@
+import { assertType, test, describe } from "vitest";
 import { makeMatrix } from "../index";
 
-const expectType = <Type>(_: Type) => void _;
-
 describe("assert types", () => {
-  it("resolves to the correct type", () => {
-    expectType<string[][][][]>(makeMatrix([1, 1, 1, 1], "hello"));
-    expectType<number[][]>(makeMatrix([4, 5], ([a]) => a));
-    expectType<string[]>(makeMatrix([4], v => v[0].toString()));
-    expectType<unknown[]>(makeMatrix([] as number[]));
+  test("resolves to the correct type", () => {
+    assertType<string[][][][]>(makeMatrix([1, 1, 1, 1], "hello"));
+    assertType<number[][]>(makeMatrix([4, 5], ([a]) => a));
+    assertType<string[]>(makeMatrix([4], v => v[0].toString()));
+    assertType<unknown[]>(makeMatrix([] as number[]));
 
-    expectType<(0 | 1)[]>(
+    assertType<(0 | 1)[]>(
       makeMatrix([1], () => (Math.random() > 0.5 ? (0 as const) : (1 as const)))
     );
 
-    expectType<{ x: number; y: number; z: number }[][][]>(
+    assertType<{ x: number; y: number; z: number }[][][]>(
       makeMatrix([4, 5, 6], ([x, y, z]) => ({ x, y, z }))
     );
   });
